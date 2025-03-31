1a51191b430d956959e1623cc1152829f4347c5b
SeungsooChoi <news2651@naver.com>
2025-03-31 19:47:00 +0900

    fix: 프론트 1차 정리

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['images.unsplash.com','picsum.photos'], // 허용할 이미지 호스트 도메인 추가
-  },
 };
 
 export default nextConfig;
